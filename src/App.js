a5fc176b2d56688ad3045934049a8f9c05f3e48a
Oliver Lin <linchong223@gmail.com>
2019-09-02 11:00:36 +0800

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ProductionLine from './containers/ProductionLine'
+import ResourcePanel from './containers/ResourcePanel'
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <StyledLayout>
+      <div className='production-line'>
+        <ProductionLine />
+      </div>
+      <div className='resource-panel'>
+        <ResourcePanel />
+      </div>
+    </StyledLayout>
+  )
 }
 
-export default App;
+export default App
+
+const StyledLayout = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  .production-line{
+    flex: 1;
+    background: blue;
+  }
+  .resource-panel{
+    flex: 0 0 200px;
+    background: red;
+  }
+`
