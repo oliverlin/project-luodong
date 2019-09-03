c089cc525486d39a1b0594902db51537fe5c1366
ocowchun <ocowchun@gmail.com>
2019-09-03 10:08:16 +0800

    refactor game

---
+++ src/engine/issueGenerator.js
@@ -0,0 +1,24 @@
+const Issue = require('./issue')
+
+class IssueGenerator {
+  constructor(minimumNewIssueCycle) {
+    this.seed = 1
+    this.minimumNewIssueCycle = minimumNewIssueCycle
+    this.no = 0
+  }
+
+  generate(currentTime) {
+    if (this.seed > Math.random()) {
+      const issue = Issue.newIssue(currentTime)
+      issue.no = this.no
+      this.no += 1
+      this.seed = this.minimumNewIssueCycle * -0.1
+      return issue
+    } else {
+      this.seed += 0.1
+      return null
+    }
+  }
+}
+
+module.exports = IssueGenerator
