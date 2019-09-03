889ff0feb140374cff4802f0fb2d809145d649e3
ocowchun <ocowchun@gmail.com>
2019-09-03 15:43:24 +0800

    Updates

---
+++ precache-manifest.a0b2aadc8e7a34491ab86f91c0c03f46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82bfda71446bc45387cabe47eb764902",
+    "url": "/project-luodong/index.html"
+  },
+  {
+    "revision": "79f813fb0a0cad5e27d1",
+    "url": "/project-luodong/static/css/main.707b1a20.chunk.css"
+  },
+  {
+    "revision": "219318e06acb7a19372b",
+    "url": "/project-luodong/static/js/2.6ef5d920.chunk.js"
+  },
+  {
+    "revision": "79f813fb0a0cad5e27d1",
+    "url": "/project-luodong/static/js/main.f04845fc.chunk.js"
+  },
+  {
+    "revision": "c9cd9bd7607d4b4f81fc",
+    "url": "/project-luodong/static/js/runtime~main.06ec647e.js"
+  }
+]);
